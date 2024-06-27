828b5edb8b9d96e2fe4d1f790ff1cbce042e1ee5
kernaltrap8 <kernaltrap@gmail.com>
2024-06-27 17:39:05 -0500

    place temporary files in work directory
    Signed-off-by: kernaltrap8 <kernaltrap@gmail.com>

--- src/badvideo.sh
+++ src/badvideo.sh
@@ -9,15 +9,14 @@ VIDEO_INPUT="$1"
 NUM_MP3_PASSES=10
 NUM_MP4_PASSES=2
 VIDEO_INPUT_NOEXT="${VIDEO_INPUT%.*}"
-VIDEO_INPUT_CONVERTED="${VIDEO_INPUT_NOEXT}_converted.mp4"
-VIDEO_NO_AUDIO="${VIDEO_INPUT_NOEXT}_no_audio.mp4"
-OUTPUT_AAC="${VIDEO_INPUT_NOEXT}_output.aac"
-OUTPUT_MP3="${VIDEO_INPUT_NOEXT}_compressed.mp3"
-OUTPUT_AMR="${VIDEO_INPUT_NOEXT}_compressed.amr"
-OUTPUT_MP4="${VIDEO_INPUT_NOEXT}_compressed.mp4"
-FINAL_MP4="${VIDEO_INPUT_NOEXT}_final.mp4"
+VIDEO_INPUT_CONVERTED="$PWD/work/${VIDEO_INPUT_NOEXT}_converted.mp4"
+VIDEO_NO_AUDIO="$PWD/work/${VIDEO_INPUT_NOEXT}_no_audio.mp4"
+OUTPUT_AAC="$PWD/work/${VIDEO_INPUT_NOEXT}_output.aac"
+OUTPUT_MP3="$PWD/work/${VIDEO_INPUT_NOEXT}_compressed.mp3"
+OUTPUT_MP4="$PWD/work/${VIDEO_INPUT_NOEXT}_compressed.mp4"
+FINAL_MP4="$PWD/${VIDEO_INPUT_NOEXT}_final.mp4"
 
-VERSION="1.4"
+VERSION="2.0"
 PREFIX="\033[37m[\033[0m\033[35m * \033[0m\033[37m]\033[0m"
 
 # Argument checking
@@ -39,7 +38,9 @@ fi
 
 # Compression jobs
 
-rm -f "$VIDEO_INPUT_CONVERTED" "${VIDEO_INPUT_NOEXT}_no_audio.mp4" "${VIDEO_INPUT_NOEXT}_output.aac" "$OUTPUT_MP3" "$OUTPUT_MP4"
+rm -rf work
+mkdir -p work
+echo -e "$VIDEO_INPUT_CONVERTED\n" "$VIDEO_NO_AUDIO\n" "$OUTPUT_AAC\n" "$OUTPUT_MP3\n" "$OUTPUT_MP4\n" "$FINAL_MP4\n"
 echo -e "$PREFIX Converting input to workable format..."
 sleep 1
 ffmpeg -y -v quiet -stats -i "$VIDEO_INPUT" -c:v copy -c:a aac -strict experimental "$VIDEO_INPUT_CONVERTED"
@@ -53,7 +54,7 @@ echo -e "$PREFIX Compressing audio stream..."
 sleep 1
 for (( i=1; i<=$NUM_MP3_PASSES; i++ ))
 do
-	OUTPUT_MP3="${VIDEO_INPUT_NOEXT}_${i}.mp3"
+	OUTPUT_MP3="work/${VIDEO_INPUT_NOEXT}_${i}.mp3"
 	ffmpeg -y -v quiet -stats -i "$OUTPUT_AAC" -c:a libmp3lame -crf 51 -b:a 8 -q 9 -preset veryfast "$OUTPUT_MP3"
 	
 	if [ $i -gt 1 ]; then
@@ -66,7 +67,7 @@ echo -e "$PREFIX Compressing video stream..."
 sleep 1
 for (( i=1; i<=$NUM_MP4_PASSES; i++ ))
 do
-	OUTPUT_MP4="${VIDEO_INPUT_NOEXT}_${i}.mp4"
+	OUTPUT_MP4="work/${VIDEO_INPUT_NOEXT}_${i}.mp4"
 	ffmpeg -y -v quiet -stats -i "$VIDEO_NO_AUDIO" -c:v libx264 -crf 51 -b:v 50 -preset veryfast "$OUTPUT_MP4"
 
 	if [ $i -gt 1 ]; then
