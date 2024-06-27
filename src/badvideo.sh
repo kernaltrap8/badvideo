8accccd4e6e4fc18dd217bf1cfb044fb43e73d84
kernaltrap8 <kernaltrap@gmail.com>
2024-06-27 14:08:23 -0500

    initial version, 1.0
    Signed-off-by: kernaltrap8 <kernaltrap@gmail.com>

---
+++ src/badvideo.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# badvideo Copyright (C) 2024 kernaltrap8
+# This program comes with ABSOLUTELY NO WARRANTY
+# This is free software, and you are welcome to redistribute it
+# under certain conditions
+
+VIDEO_INPUT="$1"
+if [[ $VIDEO_INPUT != *.mp4  ]]; then
+	VIDEO_INPUT="$1.mp4"
+fi
+NUM_MP3_PASSES=10
+VIDEO_INPUT_NOEXT="${VIDEO_INPUT%.*}"
+VIDEO_NO_AUDIO="${VIDEO_INPUT_NOEXT}_no_audio.mp4"
+OUTPUT_AAC="${VIDEO_INPUT_NOEXT}_output.aac"
+OUTPUT_MP3="${VIDEO_INPUT_NOEXT}_compressed.mp3"
+OUTPUT_MP4="${VIDEO_INPUT_NOEXT}_compressed.mp4"
+FINAL_MP4="${VIDEO_INPUT_NOEXT}_final.mp4"
+
+VERSION="1.0"
+PREFIX="\033[37m[\033[0m\033[35m * \033[0m\033[37m]\033[0m"
+
+# Argument checking
+
+if [ "$1" == "-v" ] || [ "$1" == "--version" ]; then
+	echo "badvideo v$VERSION"
+	exit 0
+fi
+
+if [ "$#" -eq 0 ]; then
+  echo -e "No arguments supplied.\nPlease supply the input filename."
+  exit 1
+fi
+
+if [ "$#" -ne 1 ]; then
+  echo -e "Incorrect number of arguments.\nUsage: $0 <input>"
+  exit 1
+fi
+
+# Compression jobs
+
+rm -f "$VIDEO_NO_AUDIO" "$OUTPUT_AAC" "$OUTPUT_MP3" "$OUTPUT_MP4" "$FINAL_MP4"
+echo -e "$PREFIX Copying audio stream to external AAC..."
+sleep 1
+ffmpeg -v quiet -stats -i "$VIDEO_INPUT" -vn -acodec copy "$OUTPUT_AAC"
+echo -e "$PREFIX Removing audio stream from video..."
+sleep 1
+ffmpeg -v quiet -stats -i "$VIDEO_INPUT" -an -c:v copy "$VIDEO_NO_AUDIO"
+echo -e "$PREFIX Compressing audio stream..."
+sleep 1
+for (( i=1; i<=$NUM_MP3_PASSES; i++ ))
+do
+	OUTPUT_MP3="${VIDEO_INPUT_NOEXT}_${i}.mp3"
+	ffmpeg -v quiet -stats -i "$OUTPUT_AAC" -c:a libmp3lame -crf 51 -b:a 8 -q 9 -preset veryfast "$OUTPUT_MP3"
+
+	if [ $i -gt 1 ]; then
+		rm "$OUTPUT_AAC"
+	fi
+	
+	OUTPUT_AAC="$OUTPUT_MP3"
+done
+echo -e "$PREFIX Compressing video stream..."
+sleep 1
+ffmpeg -v quiet -stats -i "$VIDEO_NO_AUDIO" -c:v libx264 -crf 51 -preset veryfast "$OUTPUT_MP4"
+echo -e "$PREFIX Combining compressed streams..."
+sleep 1
+ffmpeg -v quiet -stats -i "$OUTPUT_MP4" -i "$OUTPUT_AAC" -c:v copy -c:a aac -preset veryfast "$FINAL_MP4"
+echo -e "$PREFIX Removing work files..."
+sleep 1
+rm -f "$VIDEO_NO_AUDIO" *.aac *.mp3 "$OUTPUT_MP4"
