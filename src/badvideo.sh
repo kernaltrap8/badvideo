555e9ddc2bd19258add497a1a0d8886dc2a09efc
kernaltrap8 <kernaltrap@gmail.com>
2024-06-29 21:20:44 -0500

    change arg handling to use getopts
    Signed-off-by: kernaltrap8 <kernaltrap@gmail.com>

--- src/badvideo.sh
+++ src/badvideo.sh
@@ -7,8 +7,7 @@
 
 # Variable setup
 
-VERSION="4.8"
-VIDEO_INPUT="$1"
+VERSION="5.0"
 NUM_MP3_PASSES_DEFAULT=10
 NUM_MP4_PASSES_DEFAULT=2
 MP3_RATE_DEFAULT="20k"
@@ -17,83 +16,81 @@ DATE=$(date +'%d-%m-%y')
 DISABLE_DELETE=1
 PREFIX="\033[37m[\033[0m\033[35m * \033[0m\033[37m]\033[0m"
 WARNING_PREFIX="\033[37m[\033[0m\033[31m * \033[0m\033[37m]\033[0m"
-#DEBUG_PREFIX="\033[37m[\033[0m\033[32m DEBUG \033[0m\033[37m]\033[0m"
 PASS_PREFIX="\033[37m[\033[0m\033[32m ! \033[0m\033[37m]\033[0m"
 EXIT_PREFIX="\033[37m[\033[0m\033[31m ! \033[0m\033[37m]\033[0m"
-INPUT_DIR=$(dirname "$VIDEO_INPUT")
-INPUT_FILENAME=$(basename "$VIDEO_INPUT")
-VIDEO_INPUT_NOEXT="${INPUT_FILENAME%.*}"
-VIDEO_INPUT_CONVERTED="${WORK_DIR}${VIDEO_INPUT_NOEXT}_converted.mp4"
-VIDEO_NO_AUDIO="${WORK_DIR}${VIDEO_INPUT_NOEXT}_no_audio.mp4"
-OUTPUT_AAC="${WORK_DIR}${VIDEO_INPUT_NOEXT}_output.aac"
-OUTPUT_MP3="${WORK_DIR}${VIDEO_INPUT_NOEXT}_compressed.opus"
-OUTPUT_MP4="${WORK_DIR}${VIDEO_INPUT_NOEXT}_compressed.mp4"
-FINAL_MP4="./${VIDEO_INPUT_NOEXT}_final.mp4"
 
 # Function definitions
 check_args() {
-	if [ "$#" -eq 0 ]; then
-	  echo -e "No arguments supplied.\nPlease supply at least the input filename.\nUsage: $0 <input> [mp3_passes] [mp4_passes] [mp3_rate] [mp4_rate]"
-	  exit 1
+	while getopts ":vhd" opt; do
+		case $opt in
+			v)
+				echo -e "badvideo v$VERSION\nThis program is licensed under the BSD-3-Clause license.\nThe license document can be viewed here: https://opensource.org/license/bsd-3-clause"
+				exit 0
+				;;
+			h)
+				echo -e "Usage: $0 [-d] [-v] [-h] <input> [mp3_passes] [mp4_passes] [mp3_rate] [mp4_rate]"
+				exit 0
+				;;
+			d)
+				DISABLE_DELETE=0
+				;;
+			\?)
+				echo "Invalid option: -$OPTARG" >&2
+				exit 1
+				;;
+		esac
+	done
+	shift $((OPTIND -1))
+
+	if [ "$#" -lt 1 ]; then
+		echo -e "No input file supplied.\nPlease supply at least the input filename.\nUsage: $0 [-d] [-v] [-h] <input> [mp3_passes] [mp4_passes] [mp3_rate] [mp4_rate]"
+		exit 1
 	fi
-	
-	if [[ "$1" == "-v" ]] || [[ "$1" == "--version" ]]; then
-	  echo -e "badvideo v$VERSION\nThis program is licensed under the BSD-3-Clause license.\nThe license document can be viewed here: https://opensource.org/license/bsd-3-clause"
-	  exit 0
-	fi
-	
-	if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
-	  echo -e "Tip! You can also specify the passes to do and the bitrates!\nExample: $0 filename.mp4 5 5 20k 100k\nThis will set the mp3 passes to 5, the mp4 passes to 5, the mp3 bitrate to 20, and mp4 bitrate to 100."
-	  exit 0
-	fi
-	
-	if [[ "$1" == "-d" ]] || [[ "$1" == "--disable-delete" ]]; then
-	  DISABLE_DELETE=0
-	  shift
-	fi
-	
+
+	VIDEO_INPUT="$1"
+
 	if [ "$#" -ge 2 ]; then
-	   NUM_MP3_PASSES="$2"
+		NUM_MP3_PASSES="$2"
 	else
-	   NUM_MP3_PASSES="$NUM_MP3_PASSES_DEFAULT"
+		NUM_MP3_PASSES="$NUM_MP3_PASSES_DEFAULT"
 	fi
 		
 	if [ "$#" -ge 3 ]; then
-	  NUM_MP4_PASSES="$3"
+		NUM_MP4_PASSES="$3"
 	else
-	  NUM_MP4_PASSES="$NUM_MP4_PASSES_DEFAULT"
+		NUM_MP4_PASSES="$NUM_MP4_PASSES_DEFAULT"
 	fi
 		
 	if [ "$#" -ge 4 ]; then
-	  MP3_RATE="$4"
+		MP3_RATE="$4"
 	else
-	  MP3_RATE="$MP3_RATE_DEFAULT"
+		MP3_RATE="$MP3_RATE_DEFAULT"
 	fi
 		
 	if [ "$#" -ge 5 ]; then
-	  MP4_RATE="$5"
+		MP4_RATE="$5"
 	else
-	  MP4_RATE="$MP4_RATE_DEFAULT"
+		MP4_RATE="$MP4_RATE_DEFAULT"
 	fi
 }
 
 cleanup() {
 	if [[ "$DISABLE_DELETE" -eq 1 ]]; then
-	  echo -e "$WARNING_PREFIX Removing work files..."
-	  sleep 1
-	  rm -rf "$WORK_DIR"
+		echo -e "$WARNING_PREFIX Removing work files..."
+		sleep 1
+		rm -rf "$WORK_DIR"
 	fi
 	echo -e "$EXIT_PREFIX Exiting."
 	exit 1
 }
 
 check_bitrate_format() {
-    local rate="$1"
-    if [[ "$rate" =~ ^[0-9]+[kK]$ ]]; then
-        return 0  # Valid bitrate format
-    else
-        return 1  # Invalid bitrate format
-    fi
+	local rate="$1"
+	if [[ "$rate" =~ ^[0-9]+[kK]$ ]]; then
+		return 0  # Valid bitrate format
+	else
+		return 1  # Invalid bitrate format
+	fi
 }
 
 die() {
@@ -111,46 +108,54 @@ panic() {
 	fi
 	# Ensure NUM_MP3_PASSES and NUM_MP4_PASSES are valid integers
 	if ! [[ "$NUM_MP3_PASSES" =~ ^[0-9]+$ ]]; then
-	  echo "Error: NUM_MP3_PASSES must be an integer."
-	  exit 1
+		echo "Error: NUM_MP3_PASSES must be an integer."
+		exit 1
 	fi
 	
 	if ! [[ "$NUM_MP4_PASSES" =~ ^[0-9]+$ ]]; then
-	  echo "Error: NUM_MP4_PASSES must be an integer."
-	  exit 1
+		echo "Error: NUM_MP4_PASSES must be an integer."
+		exit 1
 	fi
 	
 	# Check MP3_RATE format
 	if ! check_bitrate_format "$MP3_RATE"; then
-	    echo "Error: MP3 bitrate must end with 'k' or 'K'. Example: 128k"
-	    exit 1
+		echo "Error: MP3 bitrate must end with 'k' or 'K'. Example: 128k"
+		exit 1
 	fi
 	
 	# Check MP4_RATE format
 	if ! check_bitrate_format "$MP4_RATE"; then
-	    echo "Error: MP4 bitrate must end with 'k' or 'K'. Example: 500k"
-	    exit 1
+		echo "Error: MP4 bitrate must end with 'k' or 'K'. Example: 500k"
+		exit 1
 	fi
 	if ! [ -f "$VIDEO_INPUT" ]; then
-		echo -e "$EXIT_PREFIX Input file $VIDEO_INPUT doesnt exist!"
+		echo -e "$EXIT_PREFIX Input file $VIDEO_INPUT doesn't exist!"
 		echo -e "$EXIT_PREFIX Exiting."
 		exit 1
 	fi
 }
 
 badvideo() {
+	# Variable setup for jobs
+	INPUT_DIR=$(dirname "$VIDEO_INPUT")
+	INPUT_FILENAME=$(basename "$VIDEO_INPUT")
+	VIDEO_INPUT_NOEXT="${INPUT_FILENAME%.*}"
+	VIDEO_INPUT_CONVERTED="${WORK_DIR}${VIDEO_INPUT_NOEXT}_converted.mp4"
+	VIDEO_NO_AUDIO="${WORK_DIR}${VIDEO_INPUT_NOEXT}_no_audio.mp4"
+	OUTPUT_AAC="${WORK_DIR}${VIDEO_INPUT_NOEXT}_output.aac"
+	OUTPUT_MP3="${WORK_DIR}${VIDEO_INPUT_NOEXT}_compressed.opus"
+	OUTPUT_MP4="${WORK_DIR}${VIDEO_INPUT_NOEXT}_compressed.mp4"
+	FINAL_MP4="./${VIDEO_INPUT_NOEXT}_final.mp4"
 	# Construct paths for output files
 	if [[ "$INPUT_DIR" == "." ]]; then
-	    WORK_DIR="./work_$DATE/"
+		WORK_DIR="./work_${VIDEO_INPUT_NOEXT}_$DATE/"
 	else
-	    WORK_DIR="${INPUT_DIR}/work_$DATE/"
+		WORK_DIR="${INPUT_DIR}/work_${VIDEO_INPUT_NOEXT}_$DATE/"
 	fi
-	
 	# Remove existing output the tool may have made
 	rm -f "$FINAL_MP4"
 	# Ensure the work directory exists
 	mkdir -p "$WORK_DIR"
-	
 	# Compression jobs
 	echo -e "$PREFIX Converting input to workable format..."
 	sleep 1
@@ -165,34 +170,34 @@ badvideo() {
 	sleep 1
 	for (( i=1; i<=NUM_MP3_PASSES; i++ ))
 	do
-	  OUTPUT_MP3="${WORK_DIR}${VIDEO_INPUT_NOEXT}_${i}.opus"
-	  echo -e "$PASS_PREFIX Pass: ${i}"
-	  ffmpeg -y -v quiet -stats -i "$OUTPUT_AAC" -c:a libopus -ac 1 -ar 16000 -b:a "$MP3_RATE" -vbr constrained "$OUTPUT_MP3" || die
-	  if [ $i -gt 1 ]; then
-	    rm "$OUTPUT_AAC"
-	  fi
-	  OUTPUT_AAC="$OUTPUT_MP3"
+		OUTPUT_MP3="${WORK_DIR}${VIDEO_INPUT_NOEXT}_${i}.opus"
+		echo -e "$PASS_PREFIX Pass: ${i}"
+		ffmpeg -y -v quiet -stats -i "$OUTPUT_AAC" -c:a libopus -ac 1 -ar 16000 -b:a "$MP3_RATE" -vbr constrained "$OUTPUT_MP3" || die
+		if [ $i -gt 1 ]; then
+			rm "$OUTPUT_AAC"
+		fi
+		OUTPUT_AAC="$OUTPUT_MP3"
 	done
 	echo -e "$PREFIX Compressing video stream..."
 	sleep 1
 	for (( i=1; i<=NUM_MP4_PASSES; i++ ))
 	do
-	  OUTPUT_MP4="${WORK_DIR}${VIDEO_INPUT_NOEXT}_${i}.mp4"
-	  echo -e "$PASS_PREFIX Pass: ${i}"
-	  ffmpeg -y -v quiet -stats -i "$VIDEO_NO_AUDIO" -c:v libx264 -b:v "$MP4_RATE" -preset veryfast "$OUTPUT_MP4" || die
-	  if [ $i -gt 1 ]; then
-	    rm "$VIDEO_NO_AUDIO"
-	  fi
-	  VIDEO_NO_AUDIO="$OUTPUT_MP4"
+		OUTPUT_MP4="${WORK_DIR}${VIDEO_INPUT_NOEXT}_${i}.mp4"
+		echo -e "$PASS_PREFIX Pass: ${i}"
+		ffmpeg -y -v quiet -stats -i "$VIDEO_NO_AUDIO" -c:v libx264 -b:v "$MP4_RATE" -preset veryfast "$OUTPUT_MP4" || die
+		if [ $i -gt 1 ]; then
+			rm "$VIDEO_NO_AUDIO"
+		fi
+		VIDEO_NO_AUDIO="$OUTPUT_MP4"
 	done
 	echo -e "$PREFIX Combining compressed streams..."
 	sleep 1
 	ffmpeg -y -v quiet -stats -i "$OUTPUT_MP4" -i "$OUTPUT_MP3" -c:v libx264 -c:a copy -preset veryfast "$FINAL_MP4" || die
 	echo -e "$PREFIX File outputted to ${FINAL_MP4##*/}"
 	if [ "$DISABLE_DELETE" -eq 1 ]; then
-	  echo -e "$WARNING_PREFIX Removing work files..."
-	  sleep 1
-	  rm -rf "$WORK_DIR"
+		echo -e "$WARNING_PREFIX Removing work files..."
+		sleep 1
+		rm -rf "$WORK_DIR"
 	fi
 }
 
@@ -202,7 +207,7 @@ check_args "$@"
 # If the user ends the script prematurely, make sure to cleanup any previous files
 trap cleanup SIGINT
 
-# Exit if the user provies invalid input
+# Exit if the user provides invalid input
 panic
 
 # Run the jobs
