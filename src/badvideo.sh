9a2e6e7f48cf8f683bf1ecdd4104d2e55ebf6af0
kernaltrap8 <kernaltrap@gmail.com>
2024-07-01 11:01:31 -0500

    Add more verbosity during script startup
    Signed-off-by: kernaltrap8 <kernaltrap@gmail.com>

--- src/badvideo.sh
+++ src/badvideo.sh
@@ -6,7 +6,7 @@
 # under certain conditions
 
 # Variable setup
-VERSION="5.2"
+VERSION="5.2a"
 NUM_MP3_PASSES=10
 NUM_MP4_PASSES=2
 MP3_RATE="20k"
@@ -197,6 +197,15 @@ badvideo() {
 	done
 	echo "Ruin your videos in SECONDS!"
 	echo -e "$START_PREFIX Starting badvideo v$VERSION"
+	NUM_MP3_PASSES=10
+	NUM_MP4_PASSES=2
+	MP3_RATE="20k"
+	MP4_RATE="50k"
+	if [[ "$MP3_RATE" == "20k" && "$MP4_RATE" == "50k" && "$NUM_MP3_PASSES" == 10 && "$NUM_MP4_PASSES" == 2 ]]; then
+		echo -e "$START_PREFIX Using default settings"
+	else
+		echo -e "$START_PREFIX Using settings: MP3_RATE: ${MP3_RATE}, MP4_RATE: ${MP4_RATE}, MP3_PASSES: ${NUM_MP3_PASSES}, MP4_PASSES: ${NUM_MP4_PASSES}"
+	fi
 	# Variable setup for jobs
 	INPUT_DIR=$(dirname "$VIDEO_INPUT")
 	INPUT_FILENAME=$(basename "$VIDEO_INPUT")
